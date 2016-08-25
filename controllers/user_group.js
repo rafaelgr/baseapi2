871e017e7070f62963aedb324c57b349031a92ec
Rafael Garcia (Ariadna) <rafa@myariadna.com>
2016-08-25 14:26:49 +0200

    test(Testing user groups (1)):

--- controllers/user_group.js
+++ controllers/user_group.js
@@ -4,5 +4,29 @@
 */
 var express = require('express');
 var router = express.Router();
+var userGroupDb = require('asw_basepkg').userGroup; // to access mysql db
+
+router.get('/', function (req, res) {
+    var test = req.query.test && (req.query.test == 'true');
+    userGroupDb.get(function (err, groups) {
+        if (err) {
+            res.status(500).send(err.message);
+        } else {
+            res.json(groups);
+        }
+    }, test);
+});
+
+router.post('/', function (req, res) {
+    var test = req.query.test && (req.query.test == 'true');
+    var userGroup = req.body;
+    userGroupDb.post(userGroup, function (err, group) {
+        if (err) {
+            res.status(500).send(err.message);
+        } else {
+            res.json(group);
+        }
+    }, test);
+});
 
 module.exports = router;
--- test/user_group.test.js
+++ test/user_group.test.js
@@ -1,3 +1,77 @@
 /*
  * user_group.test.js
 */
+
+var chai = require('chai'),
+    chaiHttp = require('chai-http'),
+    bpkg = require('asw_basepkg'),
+    app = require('../app').app;
+var expect = chai.expect;
+
+describe("User Group API Test", function () {
+    var tId = 0;
+    before(function (done) {
+        // prepare some records
+        bpkg.dbCon.execSql('prepare_user_group_test.sql', function (err) {
+            expect(err).to.be.null;
+            done();
+        }, true);
+    });
+    it("GET should return some user groups", function (done) {
+        chai.request(app)
+            .get('/api/user_group/?test=true')
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.a.property("status", 200);
+                expect(res).to.have.length;
+                done();
+            });
+    });
+    it("POST should create a user group", function (done) {
+        chai.request(app)
+            .post('/api/user_group/?test=true')
+            .send({
+                id: 0,
+                name: "TestUserGroup"
+            })
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.a.property("body");
+                var g = res.body;
+                expect(g).to.have.a.property("id");
+                tId = res.id;
+                done();
+            });
+    });
+    it("GET/id should return the user group with this id ", function (done) {
+        /*
+        chai.request(app)
+            .get('/api/user_group/' + tId + '?test=true')
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.a.property("status", 200);
+                expect(res).to.have.a.property("body");
+                var gs = res.body;
+                expect(gs).to.have.length(1);
+                var t = gs[0];
+                var expected = {
+                    id: tId,
+                    name: "TestUserGroup"
+                }
+                expect(t).to.deep.equal(expected);
+                done();
+            });
+            */
+            done();
+    });
+    it("PUT/id should modify the user group with that id");
+    it("DELETE/id should delete the user group with that id");
+    after(function (done) {
+        // after all delete records
+        bpkg.dbCon.execSql('delete_test.sql', function (err) {
+            expect(err).to.be.null;
+            done();
+        }, true);
+    })
+});
+
